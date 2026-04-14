0406126a8f487b8e3caa9924eae9a759905ceb38
szTheory <szTheory@users.noreply.github.com>
2026-04-14 13:03:20 -0400

    test(17-01): add failing tests for register_user_multi/1
    - Add register_user_multi/1 describe block with 5 pure-builder tests
    - Update existing register/3 tests to stub :transact instead of :insert
    - Extend MockRepo.Behaviour with transact/1 callback

--- test/support/mock_repo_behaviour.ex
+++ test/support/mock_repo_behaviour.ex
@@ -11,6 +11,8 @@ defmodule Sigra.MockRepo.Behaviour do
   @callback delete!(struct()) :: struct()
   @callback get(module(), term()) :: struct() | nil
   @callback transaction(Ecto.Multi.t()) :: {:ok, map()} | {:error, atom(), term(), map()}
+  @callback transact(Ecto.Multi.t()) ::
+              {:ok, map()} | {:error, atom(), term(), map()} | {:error, term()}
   @callback delete_all(Ecto.Queryable.t()) :: {non_neg_integer(), nil | [term()]}
   @callback all(Ecto.Queryable.t()) :: [struct()]
   @callback update_all(Ecto.Queryable.t(), keyword()) :: {non_neg_integer(), nil | [term()]}
